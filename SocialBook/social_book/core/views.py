4f0b8df3388af445945f202bbaf7c204d13d4b5e
RanjaniKathiresan <ranjanikathiresan2012@gmail.com>
2024-05-22 08:59:30 +0530

    Done with login and logout fuctionality

--- SocialBook/social_book/core/views.py
+++ SocialBook/social_book/core/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import auth, User
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
 
+@login_required(login_url='/signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -37,3 +39,23 @@ def signup(request):
             return redirect('signup')
     else:
         return render(request, 'signup.html') 
+    
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+@login_required(login_url='/signin')   
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
--- SocialBook/social_book/social_book/settings.py
+++ SocialBook/social_book/social_book/settings.py
@@ -131,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# LOGIN_URL = '/signin'
+
+# SESSION_ENGINE = 'django.contrib.sessions.backends.db'
