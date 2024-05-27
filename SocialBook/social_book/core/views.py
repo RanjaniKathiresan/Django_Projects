e7a4770e8b6887581f5d6a226d00c8436353cfb5
RanjaniKathiresan <ranjanikathiresan2012@gmail.com>
2024-05-27 07:31:16 +0530

    search and follow recommendation feature

--- SocialBook/social_book/core/views.py
+++ SocialBook/social_book/core/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import auth, User
 from django.contrib.auth.decorators import login_required
 from .models import FollowersCount, Profile, Post, LikePost
 from itertools import chain
+import random
 
 # Create your views here.
 
@@ -26,9 +27,33 @@ def index(request):
         feed.append(feed_lists)
     
     feed_list = list(chain(*feed))
+    
+    # user suggestion starts
+    all_users = User.objects.all()
+    user_following_all = []
 
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile':user_profile, 'posts':feed_list})
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    new_suggestions_list = [x for x in list(all_users) if x not in list(user_following_all)]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if x not in list(current_user)]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+    
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+    
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, 'index.html', {'user_profile':user_profile, 'posts':feed_list, 'suggestions_username_profile_list':suggestions_username_profile_list})
 
 @login_required(login_url='/signin')
 def settings(request):
@@ -55,6 +80,27 @@ def settings(request):
             user_profile.save()
         return redirect('settings')
     return render(request, 'setting.html', {'user_profile':user_profile})
+
+@login_required(login_url='/signin')
+def search(request):
+    user_objects = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_objects)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+
+        username_profile = []
+        username_profile_list = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+        username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, 'search.html', {'user_profile':user_profile, 'username_profile_list':username_profile_list})
 
 def signup(request):
     if request.method == 'POST':
